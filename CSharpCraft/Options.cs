b6629ab20d36d03b73f781ddb5860fca01f8c074
elwiiwii <elwiiwiiwoowoo@gmail.com>
2024-07-16 18:43:32 +0100

    save

--- CSharpCraft/Options.cs
+++ CSharpCraft/Options.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Threading.Channels;
 using System.Transactions;
 using Color = Microsoft.Xna.Framework.Color;
 
@@ -17,7 +19,7 @@ namespace CSharpCraft
         private bool menuSelectedX;
         private int menuSelectedY;
         private int currentMenuOption;
-        private int[] menuOptions = [1, 2, 3];
+        private int[] menuOptions = new int[4];
 
         private static string[] keyboardBinds1 = ["left"];
         private static string[] keyboardBinds2 = ["left"];
@@ -81,7 +83,7 @@ namespace CSharpCraft
             if (p8.Btnp(2)) { currentMenuOption -= 1; }
             if (p8.Btnp(3)) { currentMenuOption += 1; }
             
-            menuOptions[1] = Loop(menuOptions[1]);
+            menuOptions[currentMenuOption] = Loop(menuOptions[currentMenuOption]);
         }
 
         public void Draw()
@@ -99,8 +101,8 @@ namespace CSharpCraft
             Vector2 size = new(cellW, cellH);
 
             batch.Draw(textureDictionary["OptionsMenuBackground"], new Vector2(0, 0), null, Color.White, 0, Vector2.Zero, size, SpriteEffects.None, 0);
-            batch.Draw(textureDictionary["Arrow"], new Vector2(0 * cellW, 1 * cellH), null, p8.colors[5], 0, Vector2.Zero, size, SpriteEffects.None, 0);
-            p8.Print("back", 5, 1, 5);
+            batch.Draw(textureDictionary["Arrow"], new Vector2(0 * cellW, 1 * cellH), null, p8.colors[currentMenuOption == 0 ? 7 : 5], 0, Vector2.Zero, size, SpriteEffects.None, 0);
+            p8.Print("back", 5, 1, currentMenuOption == 0 ? 7 : 5);
             //batch.Draw(textureDictionary["OptionsMenuTab"], new Vector2(15 * cellW, 22 * cellH), null, Color.White, 0, Vector2.Zero, size, SpriteEffects.None, 0);
 
             //batch.Draw(textureDictionary["SelectorHalf"], new Vector2(16 * cellW, 31 * cellH), null, Color.White, 0, Vector2.Zero, size, SpriteEffects.None, 0);
@@ -118,30 +120,45 @@ namespace CSharpCraft
             {
                 p8.Print(options.Keys.ElementAt(i), 19 + 58 * i, 19 - Math.Abs(i + menuOptions[1]), 7);
             }
-            
-            if (currentMenuOption == 1)
-            {
-                if (menuOptions[1] == 0)
-                {
-                    batch.Draw(textureDictionary["KeybindsMenu"], new Vector2(8 * cellW, 46 * cellH), null, Color.White, 0, Vector2.Zero, size, SpriteEffects.None, 0);
-                    batch.Draw(textureDictionary["SelectorHalf"], new Vector2(16 * cellW, 31 * cellH), null, Color.White, 0, Vector2.Zero, size, SpriteEffects.None, 0);
-                    batch.Draw(textureDictionary["SelectorHalf"], new Vector2(31 * cellW, 31 * cellH), null, Color.White, 0, Vector2.Zero, size, SpriteEffects.FlipHorizontally, 0);
 
-                    for (int i = 0; i <= 1; i++)
-                    {
-                        p8.Print(controlsOptions.Keys.ElementAt(i), 19 + 54 * i, 33, 7);
-                    }
-                }
-            }
-            else if (currentMenuOption == 2)
+            //if (currentMenuOption == 1)
+            //{
+            //    if (menuOptions[1] == 0)
+            //    {
+            //        batch.Draw(textureDictionary["KeybindsMenu"], new Vector2(8 * cellW, 46 * cellH), null, Color.White, 0, Vector2.Zero, size, SpriteEffects.None, 0);
+            //        batch.Draw(textureDictionary["SelectorHalf"], new Vector2(16 * cellW, 31 * cellH), null, Color.White, 0, Vector2.Zero, size, SpriteEffects.None, 0);
+            //        batch.Draw(textureDictionary["SelectorHalf"], new Vector2(31 * cellW, 31 * cellH), null, Color.White, 0, Vector2.Zero, size, SpriteEffects.FlipHorizontally, 0);
+            //
+            //        for (int i = 0; i <= 1; i++)
+            //        {
+            //            p8.Print(controlsOptions.Keys.ElementAt(i), 19 + 54 * i, 33, 7);
+            //        }
+            //    }
+            //}
+            
+            if (currentMenuOption == 2)
             {
                 if (menuOptions[2] == 0)
                 {
-                    p8.Rectfill(17, 32, 51, 38, 13);
+                    //p8.Rectfill(17, 32, 51, 38, 13);
                 }
             }
-            
-            
+            if (menuOptions[1] == 0)
+            {
+                var position2 = new Vector2((16 + Math.Abs(54 * menuOptions[2])) * cellW, 31 * cellH);
+                var position3 = new Vector2((30 + Math.Abs(62 * menuOptions[2])) * cellW, 31 * cellH);
+
+                batch.Draw(textureDictionary["SelectorHalf"], position2, null, p8.colors[currentMenuOption == 2 ? 7 : 5], 0, Vector2.Zero, size, SpriteEffects.None, 0);
+                batch.Draw(textureDictionary["SelectorHalf"], position3, null, p8.colors[currentMenuOption == 2 ? 7 : 5], 0, Vector2.Zero, size, SpriteEffects.FlipHorizontally, 0);
+                batch.Draw(textureDictionary["KeybindsMenu"], new Vector2(8 * cellW, 46 * cellH), null, Color.White, 0, Vector2.Zero, size, SpriteEffects.None, 0);
+
+                for (int i = 0; i <= 1; i++)
+                {
+                    p8.Print(controlsOptions.Keys.ElementAt(i), 19 + 54 * i, 33, 7);
+                }
+            }
+
+
 
             //int i = 0;
             //foreach (var key in keys)
