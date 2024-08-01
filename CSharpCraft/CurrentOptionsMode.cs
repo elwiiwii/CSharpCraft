e40a9cd41293df804b2e14c97823748377359700
ellie <elwiiwiiwoowoo@gmail.com>
2024-08-01 01:11:55 +0100

    save

---
+++ CSharpCraft/CurrentOptionsMode.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CSharpCraft
+{
+    public class CurrentOptionsMode
+    {
+        public int CurrentMode { get; set; } = 2;
+    }
+}
---
+++ CSharpCraft/MainOptions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Drawing;
+using System.Reflection;
+using Color = Microsoft.Xna.Framework.Color;
+
+namespace CSharpCraft
+{
+    public class MainOptions(Pico8Functions p8, Dictionary<string, Texture2D> textureDictionary, SpriteBatch batch, GraphicsDevice graphicsDevice, KeyboardOptionsFile keyboardOptionsFile, List<IGameMode> optionsModes) : IGameMode
+    {
+
+        public string GameModeName { get => "options"; }
+
+        public int currentOptionsMode;
+
+        public void Init()
+        {
+            currentOptionsMode = 2;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+            
+        }
+
+    }
+
+}
